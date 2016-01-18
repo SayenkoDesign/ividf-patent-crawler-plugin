0802df35a5dd7b267013bb4e26a967f853657f79
Robert M. Parker <rparker@yamiko.ninja>
2016-01-18 02:24:06 -0800

    bug fixes and improvements

--- web/js/ividf-patent-crawler.js
+++ web/js/ividf-patent-crawler.js
@@ -1,6 +1,15 @@
 var $ = jQuery;
 $(document).ready(function()
 {
+    function sortFields() {
+        if($('#patent_controls select[name=sort]').val() == ".applied_on" || $('#patent_controls select[name=sort]').val() == ".issued_on") {
+            sortDate($('#patent_controls select[name=sort]').val(), $('#patent_controls select[name=dir]').val());
+        } else {
+            sortText($('#patent_controls select[name=sort]').val(), $('#patent_controls select[name=dir]').val());
+        }
+        showFirstRows();
+    }
+
     // sort by data in selecot.
     // use DESC as second arg to order in desc order
     function sortText(selector, order) {
@@ -30,8 +39,12 @@ $(document).ready(function()
         var listitems = mylist.children(".patent").get();
 
         listitems.sort(function(a, b) {
-            var dateA = $(a).find(selector).text().toUpperCase().split(" ").reverse();
-            var dateB = $(b).find(selector).text().toUpperCase().split(" ").reverse();
+            var dateA = $(a).find(selector).text().trim().toUpperCase().split(" ");
+            var dateB = $(b).find(selector).text().trim().toUpperCase().split(" ");
+            dateA.shift();
+            dateB.shift();
+            dateA.reverse();
+            dateB.reverse();
 
             dateA[1] = convertMonthToDigit(dateA[1]);
             dateB[1] = convertMonthToDigit(dateB[1]);
@@ -51,7 +64,6 @@ $(document).ready(function()
         });
     }
 
-    // convert month string to digit
     function convertMonthToDigit(month) {
         switch(month) {
             case "JAN": return "01";
@@ -68,16 +80,6 @@ $(document).ready(function()
             case "DEC": return "12";
         }
     }
-
-    // listent and update table sorting
-    $('#patent_controls select').on('change', function() {
-        if($(this).val() == ".applied_on" || $(this).val() == ".issued_on") {
-            sortDate($('#patent_controls select[name=sort]').val(), $('#patent_controls select[name=dir]').val());
-        } else {
-            sortText($('#patent_controls select[name=sort]').val(), $('#patent_controls select[name=dir]').val());
-        }
-        showFirstRows();
-    });
 
     // show 10 at a time
     function showFirstRows() {
@@ -102,13 +104,53 @@ $(document).ready(function()
         }
     });
 
+    function filterRows() {
+        var term = $('#patent_controls input[name=filter]').val();
+        var pending = $('#patent_controls input[name=pending]').is(':checked');
+
+        $('.vc_row.patent').each(function(index){
+            $(this).removeClass('hidden');
+            if(term) {
+                var text = $(this).text();
+                if(text.search(new RegExp(term, "i")) === -1) {
+                    $(this).addClass('hidden');
+                }
+            }
+            if(!pending) {
+                var issued_text = $(this).find('.issued_on').text();
+                var pending_text = "Pending";
+                if (issued_text.search(new RegExp(pending_text, "i")) !== -1) {
+                    $(this).addClass('hidden');
+                }
+            }
+        });
+    }
+
+    var key_timer;
+    $('#patent_controls input[name=filter]').on('keyup', function(){
+        clearTimeout(key_timer);  //clear any running timeout on key up
+        key_timer = setTimeout(function() {
+            filterRows();
+            sortFields();
+            showFirstRows();
+        }, 500);
+    });
+
+    $('#patent_controls input[name=pending]').on('change', function(){
+        filterRows();
+        sortFields();
+        showFirstRows();
+    });
+
+    $('#patent_controls select').on('change', function() {
+        sortFields();
+    });
+
     // filter patents on submit
     $('#patent_controls').on('submit', function(e){
-        var term = $('#patent_controls input').val();
-        $('.vc_row.patent:containsi("' + term + '")').removeClass('hidden');
-        $('.vc_row.patent:not(:containsi("' + term + '"))').addClass('hidden');
+        filterRows();
+        sortFields();
         showFirstRows();
-
         e.preventDefault();
         return false;
     });
